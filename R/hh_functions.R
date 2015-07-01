a4d534f7464a3fe1a08212fde43efde7756f51bf
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-07-01 14:03:19 -0400

    Added space for household helper functions.

---
+++ R/hh_functions.R
@@ -0,0 +1,11 @@
+#' Get the market segment
+#'
+#' @param vehicles vector with the number of vehicles in the household.
+#' @parma people vector with the number of people to compare against the number
+#'   of vehicles.
+#'
+#' @return a vector with the market segment the household belongs to.
+get_segment <- function(vehicles, people, type){
+
+  ifelse(vehicles == 0, "zerocar", paste(people, type, sep = " "))
+}
